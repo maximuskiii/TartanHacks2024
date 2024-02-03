f5fc88f1640e4002906e393edf915d24692fe573
Maxim Yagnyatinskiy <115981026+maximuskiii@users.noreply.github.com>
2024-02-03 16:48:38 -0500

    Update trajectory_planning.py

--- trajectory_planning.py
+++ trajectory_planning.py
@@ -17,31 +17,24 @@ def calculate_trajectory_and_save(array, output_image_path='graph.png'):
     else:
         output_path = [tuple(row) for row in output_path]
 
-    # Convert output_path to a NumPy array for easier handling
     output_path_array = np.array(output_path)
 
-    # Load the map image (assuming 'array' contains the map image)
     map_image = array
 
-    # Create a figure and axis for plotting
-    fig, ax = plt.subplots(figsize=(6, 6))  # Adjust size as needed
+    fig, ax = plt.subplots(figsize=(6, 6))
 
     ax.set_xticks([])
     ax.set_yticks([])
 
-    # Plot the map image
     ax.imshow(map_image, cmap='Greys', origin='lower', extent=[
               0, map_image.shape[1], 0, map_image.shape[0]])
 
-    # Plot the path overlay on the map image
     x_coords, y_coords = output_path_array[:,
                                            1] + 0.5, output_path_array[:, 0] + 0.5
     ax.plot(x_coords, y_coords, 'r-', linewidth=2, marker='o', markersize=5)
     ax.set_title('Coverage Path on Grid')
 
-    # Save the figure as an image
     plt.savefig(output_image_path)
-    plt.close()  # Close the plot to free up memory
+    plt.close() 
 
-    # Optionally return the path for further use
     return output_path
