011605a89cd25de0a04fad22fd9fe83de5e26bb0
Benjamin Kleyner <54718380+benkleyner@users.noreply.github.com>
2024-02-03 16:53:49 -0500

    Update heatmap.py

--- heatmap.py
+++ heatmap.py
@@ -7,16 +7,11 @@ def load_npy(file_path):
 
 
 if __name__ == "__main__":
-    # Load the heatmap
     heatmap = load_npy("heatmap.npy")
-
-    # Define circle parameters
     center_x, center_y = heatmap.shape[0] // 2 + 13, heatmap.shape[1] // 2
     radius = (
         min(center_x, center_y) // 2 - 10
-    )  # Adjust this factor to change the size of the circle
-
-    # Apply gradient based on circle
+    ) 
     for i in range(heatmap.shape[0]):
         for j in range(heatmap.shape[1]):
             distance = np.sqrt((i - center_x) ** 2 + (j - center_y) ** 2)
@@ -29,8 +24,6 @@ if __name__ == "__main__":
 
     # apply color map
     heatmap = cv2.applyColorMap(heatmap[:, :, 0], cv2.COLORMAP_JET)
-
-    # Display the heatmap
     cv2.imshow("Heatmap", heatmap)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
