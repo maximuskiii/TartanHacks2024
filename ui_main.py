5f12744f84f9bcc8cf8af42b82475ad3a85b2d39
Maxim Yagnyatinskiy <115981026+maximuskiii@users.noreply.github.com>
2024-02-03 16:50:21 -0500

    Update ui_main.py

--- ui_main.py
+++ ui_main.py
@@ -18,7 +18,6 @@ directory = os.path.dirname(os.path.realpath(__file__))
 image_path = os.path.join(directory, "graph.png")
 
 
-# Load the .ui files directly
 MainPageUi, MainPageBase = uic.loadUiType("main_page.ui")
 NavigationDisplayUi, NavigationDisplayBase = uic.loadUiType(
     "navigation_display_page.ui"
@@ -31,20 +30,17 @@ class ImageDisplay(NavigationDisplayBase, NavigationDisplayUi):
         super().__init__(parent)
         self.setupUi(self)
 
-        # Create a QLabel to display the image
         self.image_label = QLabel(self.matplotlibWidget)
         self.image_label.setGeometry(self.matplotlibWidget.geometry())
 
         print("location: ", image_path)
 
-        # Load and display the image
         self.display_image(image_path)
 
     def display_image(self, image_path):
         print("display image called")
         pixmap = QPixmap(image_path)
         self.image_label.setPixmap(pixmap)
-        # Scale the image to fit the QLabel
         self.image_label.setScaledContents(True)
 
 
@@ -52,12 +48,11 @@ class MainPage(MainPageBase, MainPageUi):
     def __init__(self, parent=None):
         super(MainPage, self).__init__(parent)
         self.setupUi(self)
-        # Connect the submit button to the method that transitions to the navigation display
         self.submit_button.clicked.connect(self.gotoNavigationDisplay)
 
     def save_graph_image(self, image_path):
         plt.savefig(image_path)
-        plt.close()  # Close the plot to free up memory
+        plt.close()  
 
     def addr_to_coord(self, addr):
         self.geocoding = gmaps.geocode(addr)
@@ -81,10 +76,8 @@ class MainPage(MainPageBase, MainPageUi):
         style = "feature:all|element:labels|visibility:off"
         map_url = f"https://maps.googleapis.com/maps/api/staticmap?center={location}&zoom={zoom}&size={size}&maptype={maptype}&style={style}&key={api_key}"
 
-        # Fetch the map image
         response = requests.get(map_url)
         if response.status_code == 200:
-            # Convert the image to a numpy array
             image = Image.open(BytesIO(response.content))
             image_array_rgb = np.array(image)
             img = cv2.cvtColor(image_array_rgb, cv2.COLOR_RGB2BGR)
@@ -92,7 +85,6 @@ class MainPage(MainPageBase, MainPageUi):
             print("Error fetching the map image")
             return
 
-        # Image processing
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         blurred = cv2.GaussianBlur(gray, (5, 5), 0)
         edges = cv2.Canny(blurred, 50, 150)
@@ -103,32 +95,25 @@ class MainPage(MainPageBase, MainPageUi):
         black_img = np.zeros_like(img)
         cv2.drawContours(black_img, contours, -1, (255, 255, 255), 4)
 
-        # Cropping
         height, width = black_img.shape[:2]
         new_height = height - bar_height
         cropped_black_img = black_img[0:new_height, 0:width]
 
         return np.array(cropped_black_img)
 
-    # Optionally, save the processed image as a numpy array
-    # np.save(npy_array_path, cropped_black_img)  # You need to define npy_array_path
+    # np.save(npy_array_path, cropped_black_img) 
 
     def gotoNavigationDisplay(self):
-        # Getting the address from the input
         self.userText = self.address_input.toPlainText()
-        # Processing the map image to get the array
         self.array = self.process_map_image(self.userText)
-        # Assuming you save the graph as an image in a known location
-        # Modify this function to save the graph image
         # self.save_graph_image('graph.png')
         from trajectory_planning import calculate_trajectory_and_save
 
         calculate_trajectory_and_save(self.array)
         only_image = os.path.basename(image_path)
         self.imageDisplay = ImageDisplay(image_path=only_image, parent=self)
-        self.imageDisplay.show()  # Show the ImageDisplay UI
+        self.imageDisplay.show()
         print("we have passed showing")
-        # self.close()  # Close the MainPage UI
 
 
 if __name__ == "__main__":
